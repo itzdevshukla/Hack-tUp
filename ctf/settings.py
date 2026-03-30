285853a83fc7dbacfe000f6a96fbfbb75fea48dc
Dev Shukla <devs44236@gmail.com>
2026-03-30 20:52:12 +0530

    Something related to buckets

--- ctf/settings.py
+++ ctf/settings.py
@@ -218,7 +218,11 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_QUERYSTRING_AUTH = True
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+}
 
 
 AWS_S3_SIGNATURE_VERSION = "s3v4"
