efe748654b21f1c9e77ab49d0139bad4eacd5aec
Dev Shukla <devs44236@gmail.com>
2026-03-30 20:15:39 +0530

    This has some features

--- ctf/settings.py
+++ ctf/settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
     'teams',
     'channels',
     'django_ratelimit',
+    'storages',
 ]
 
 # ================= EMAIL CONFIGURATION =================
@@ -214,3 +215,25 @@ if not DEBUG:
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SESSION_COOKIE_SAMESITE = 'Lax'
+
+
+
+
+
+
+
+
+
+# AWS s3 Buckets settings
+import os
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_QUERYSTRING_AUTH = True
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
