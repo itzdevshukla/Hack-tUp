c659c4c66552f31974cd54c506c48dc46f3f0974
Dev Shukla <devs44236@gmail.com>
2026-03-30 20:49:36 +0530

    Something related to buckets

--- ctf/settings.py
+++ ctf/settings.py
@@ -220,6 +220,10 @@ AWS_QUERYSTRING_AUTH = True
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
+
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+AWS_S3_ADDRESSING_STYLE = "virtual"
+
 # ================= MEDIA FILES (ATTACHMENTS) =================
 
 MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/"
